5f8778bb7334f8fdf0cf8b69e49f1f770645fbf6
김혜리 <hyer1k@naver.com>
2020-06-05 14:44:34 +0900

    initial project

---
+++ Observable/Observable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Observable
+//
+//  Created by 김혜리 on 2020/06/05.
+//  Copyright © 2020 hyerikim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
